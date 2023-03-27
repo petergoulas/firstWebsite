ade2b9c1442552137527db15eb02b77e222ca4e8
Goulas <panagiotis.goulas@accenture.com>
2023-03-27 20:41:18 +1100

    login page-working website with music

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hello_p");
+
+
+// var mysong=document.getElementById("mysong");
+// var icon=document.getElementById("icon");
+
+// icon.onclick=function(){
+//   if(mysong.paused) {
+//     mysong.play();
+//     icon.src = "img/pause2.png"
+//   } else {
+//     mysong.pause();
+//     icon.src = "img/play.png";
+//   }
+// }
